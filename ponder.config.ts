cf1a1d8e84dff3607b3cd6d07f7bc142b44c10b2
Jem <0x0xjem@gmail.com>
2025-12-31 12:52:34 +0400

    fix: update ponder configuration to support the new version

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,18 +1,17 @@
 import { createConfig } from "ponder";
-import { http } from "viem";
 
 import { ChainlinkPriceFeedAbi } from "./abis/ChainlinkPriceFeed";
 
 export default createConfig({
-  networks: {
+  chains: {
     mainnet: {
-      chainId: 1,
-      transport: http(process.env.PONDER_RPC_URL_1),
+      id: 1,
+      rpc: process.env.PONDER_RPC_URL_1,
     },
   },
   contracts: {
     ETHUSDPriceFeed: {
-      network: "mainnet",
+      chain: "mainnet",
       abi: ChainlinkPriceFeedAbi,
       address: "0x7d4E742018fb52E48b08BE73d041C18B21de6Fb5",
       startBlock: 22000000,
